7860b4ce428cb851c4a80e23cfc07430294b99de
Jesse Denick <jessedenick@gmail.com>
2019-09-26 18:03:46 -0400

    added ajax call and was able to create runTableQuery function properly

---
+++ app/data/table-data.js
@@ -0,0 +1,10 @@
+var tableArray = [
+    {
+            customerName: "Jesse",
+            customerEmail: "jessedenick@mail.com",
+            customerID: "JesseD",
+            phoneNumber: "000-000-0000"
+    }
+];
+
+module.exports = tableArray;
---
+++ app/data/waitinglist-data.js
@@ -0,0 +1,10 @@
+var waitingArray = [
+    {
+            customerName: "Marie",
+            customerEmail: "mariedenick@mail.com",
+            customerID: "MarieD",
+            phoneNumber: "111-111-1111"
+    }
+];
+
+module.exports = waitingArray;
